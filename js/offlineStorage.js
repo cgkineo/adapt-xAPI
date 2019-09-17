627d47a735c9ab1a4ae872afeee72cd67f24759c
Dan Ghost <daniel.ghost@kineo.com>
2019-09-17 18:24:48 +0100

    Added ability to retrieve actor info based on keys used in adapt-contrib-spoor. First and last name attributes have been excluded, as an assumption would be needed due to there being no standard for the actor.name in xAPI.

--- js/offlineStorage.js
+++ js/offlineStorage.js
@@ -8,11 +8,40 @@ define([
 		model: new Backbone.Model(),
 
 		get: function(name) {
-			return this.model.get(name);
+			switch (name.toLowerCase()) {
+				case "student":// for backwards-compatibility. learnerInfo is preferred and will give more information
+					return this.model.get('actor').name;
+				case "learnerinfo":
+					return this._getActorData();
+				default:
+					return this.model.get(name);
+			}
 		},
 
 		set: function(name, value) {
 			this.model.set(name, value);
+		},
+
+		_getActorData: function() {
+			var actor = this.model.get('actor');
+
+			// don't think we should make any judgement on name format for firstname or lastname, as there is no standard for this in xAPI
+			return {
+				id: this._getIdFromActor(actor),
+				name: actor.name
+			}
+		},
+
+		_getIdFromActor: function(actor) {
+			var id = actor.account && account.name;			
+			if (id) return id;
+
+			id = actor.openid;
+			if (id) return id;
+
+			id = actor.mbox || actor.mbox_sha1sum;
+
+			return id;
 		}
 
 	};
